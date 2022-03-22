7e1acc66de4a02e94818265f60e742a0154e417d
ali-abbas27 <ali.abbas@accelirate.com>
2022-03-22 17:28:07 +0500

    Bug fix

--- auto_sent/auto_sent/settings.py
+++ auto_sent/auto_sent/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import django-heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,4 +148,4 @@ REST_FRAMEWORK = {
         'knox.auth.TokenAuthentication',
     ]
 }
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
